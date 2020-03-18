3e8f63b20a835862a738764dc5900f6f07b9b259
Royer Robin <robin.royer@qarnot-computing.com>
2020-03-18 20:54:20 +0100

    update to 2.2.2
    migrate PublicKeyToken to PublicKey for "friends assembly"

--- QarnotSDK/Tools/Utils.cs
+++ QarnotSDK/Tools/Utils.cs
@@ -8,7 +8,11 @@ using System.Net.Http;
 #if (DEBUG)
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("QarnotSDK.UnitTests")]
 #else
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("QarnotSDK.UnitTests, PublicKeyToken=498a57251e4c9100")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("QarnotSDK.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d90399a0cf9d9d"
+                                                                                            + "50a5999f3da1a6d5a5a9777d0e5faf95a10381da641b3fbf15f9ea13096eda48f1ab8498d348d3"
+                                                                                            + "d711185ed238ec17a097c0c7a39754643f9eebecb6c12ccdc767c0655845fc03c33f1ee90a7cce"
+                                                                                            + "23b6d8f40bde0bffff0d8a4cf2b0d0ea365b2f58c1293854c2f23ed7e8196a06e5e33a1e25f5fd"
+                                                                                            + "3ab01d88")]
 #endif
 
 namespace QarnotSDK {
