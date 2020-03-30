bd2e73352cb55fcb8e61a0d26f83f4d2f05976eb
Royer Robin <robin.royer@qarnot-computing.com>
2020-03-30 23:07:03 +0200

    ensure bash file is linux line return QNET-2238

--- Documentation/build_documentation.sh
+++ Documentation/build_documentation.sh
@@ -3,13 +3,19 @@ set -e
 
 expected_cur_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 cd $expected_cur_dir
-pushd ..
-msbuild /p:Configuration=Debug QarnotSDK/QarnotSDK.csproj
-if [ ! -d docfx ]; then
-    nuget install docfx.console -OutputDirectory Documentation/docfx -Version 2.49.0
-fi
-chmod 755 Documentation/docfx/docfx.console.2.49.0/tools/docfx.exe
-mono Documentation/docfx/docfx.console.2.49.0/tools/docfx.exe build Documentation/docfx.json
 
+pushd ..
+
+pushd QarnotSDK
+msbuild -t:restore
+msbuild /p:Configuration=Debug QarnotSDK.csproj
 popd
+popd
+if [ ! -d docfx ]; then
+    nuget install docfx.console -OutputDirectory docfx -Version 2.49.0
+fi
+chmod 755 docfx/docfx.console.2.49.0/tools/docfx.exe
+mono docfx/docfx.console.2.49.0/tools/docfx.exe metadata docfx.json
+mono docfx/docfx.console.2.49.0/tools/docfx.exe build docfx.json
+
 # back in toplevel directory
