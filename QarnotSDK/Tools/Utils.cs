f492792419adf228d7315f92232e6e3d3267c3fe
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2021-04-27 19:19:10 +0200

    Add public key for DynamicProxyGenAssembly2

--- QarnotSDK/Tools/Utils.cs
+++ QarnotSDK/Tools/Utils.cs
@@ -14,7 +14,7 @@ using System.Net.Http;
                                                                                             + "23b6d8f40bde0bffff0d8a4cf2b0d0ea365b2f58c1293854c2f23ed7e8196a06e5e33a1e25f5fd"
                                                                                             + "3ab01d88")]
 #endif
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 
 namespace QarnotSDK {
     internal static class Utils
