46d3c100703297045be6f66a7604ec0536c01e14
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2022-03-01 14:52:49 +0100

    v5.4.2
    Bump DocFX version

--- Documentation/build_documentation.sh
+++ Documentation/build_documentation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+docfx_version="2.58.9"
+
 expected_cur_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 cd $expected_cur_dir
 
@@ -12,10 +14,10 @@ msbuild /p:Configuration=Debug QarnotSDK.csproj
 popd
 popd
 if [ ! -d docfx ]; then
-    nuget install docfx.console -OutputDirectory docfx -Version 2.49.0
+    nuget install docfx.console -OutputDirectory docfx -Version $docfx_version
 fi
-chmod 755 docfx/docfx.console.2.49.0/tools/docfx.exe
-mono docfx/docfx.console.2.49.0/tools/docfx.exe metadata docfx.json
-mono docfx/docfx.console.2.49.0/tools/docfx.exe build docfx.json
+chmod 755 docfx/docfx.console.$docfx_version/tools/docfx.exe
+mono docfx/docfx.console.$docfx_version/tools/docfx.exe metadata docfx.json
+mono docfx/docfx.console.$docfx_version/tools/docfx.exe build docfx.json
 
 # back in toplevel directory
